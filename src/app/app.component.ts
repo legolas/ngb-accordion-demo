ebf15af1524f1938a839db999fc006b98e7b1239
Marcel Dullaart <marcel.dullaart@gmail.com>
2018-02-14 09:38:22 +0100

    Added hamburger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NgbAccordionConfig } from '@ng-bootstrap/ng-bootstrap';
+
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the NgbAccordion demo';
+    title = 'the Bootstrap 4 Angular Components demo';
 
-  constructor(config: NgbAccordionConfig) {
-    config.closeOthers = true;
-  }
+    constructor(config: NgbAccordionConfig) {
+        config.closeOthers = true;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HamburgerComponent } from './hamburger/hamburger.component';
 import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        AccordionComponent
+        AccordionComponent,
+        HamburgerComponent
     ],
     imports: [
         BrowserModule,
